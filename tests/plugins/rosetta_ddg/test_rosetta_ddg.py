095b3f8320038ea1146c6da81f2177801f643091
Alexey Strokach <alex.strokach@utoronto.ca>
2020-10-07 13:52:49 -0400

    black

--- tests/plugins/rosetta_ddg/test_rosetta_ddg.py
+++ tests/plugins/rosetta_ddg/test_rosetta_ddg.py
@@ -48,9 +48,7 @@ def test_correct_residue(structure: Path, mutation: str, is_correct: bool):
             if "ERROR: Assertion `pose.residue(resnum).name1() == wt` failed" in line:
                 raise ResidueMismatchError
 
-    with unittest.mock.patch(
-        "ev2.plugins.rosetta_ddg.rosetta_ddg.subprocess.run", subprocess_run
-    ):
+    with unittest.mock.patch("ev2.plugins.rosetta_ddg.rosetta_ddg.subprocess.run", subprocess_run):
         with pytest.raises(ResidueMatchError if is_correct else ResidueMismatchError):
             RosettaDDG.analyze_mutation(mutation, data)
 
