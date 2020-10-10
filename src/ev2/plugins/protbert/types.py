b43b2ffdecf5a77a1b115313c86ebcc68086f571
Alexey Strokach <alex.strokach@utoronto.ca>
2020-10-09 23:46:05 -0400

    Add notebooks/02_run_*

--- src/ev2/plugins/protbert/types.py
+++ src/ev2/plugins/protbert/types.py
@@ -1,6 +1,7 @@
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 class ProtBertData(NamedTuple):
     protein_sequence: str
-    ligand_sequence: str
+    ligand_sequence: Optional[str]
+    combined_sequence: Optional[str]
