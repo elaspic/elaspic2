32971173dea944304116f6242643704cceda16c1
Alexey Strokach <alex.strokach@utoronto.ca>
2020-10-24 01:58:35 -0400

    Make SVG figures resizable after collecting

--- scripts/collect_figures.sh
+++ scripts/collect_figures.sh
@@ -6,3 +6,8 @@ rsync -av \
     --include="*/" --include="*.svg" --include="*.png" --include="*.pdf" --exclude="*" \
     --prune-empty-dirs \
     ./notebooks/ ./docs/images/
+
+
+SED_PATTERN='s/<svg preserveAspectRatio="none"/CRAZYSTRINGNOTINSVG/g; s/<svg/<svg preserveAspectRatio="none"/g; s/CRAZYSTRINGNOTINSVG/<svg preserveAspectRatio="none"/g'
+
+find ./docs/images/ -name '*.svg' | xargs -i{} sed -i -e "${SED_PATTERN}" {}
