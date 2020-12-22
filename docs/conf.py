3b63a64ef9e9834c0bb29f2ba84769745ce52299
ostrokach <ostrokach@gmail.com>
2020-12-21 22:17:39 -0500

    Fix documentation versions URL

--- docs/conf.py
+++ docs/conf.py
@@ -156,7 +156,7 @@ html_theme = "msmb_theme"
 html_theme_options = {"collapse_navigation": False, "display_version": True}
 
 # Context is a dictionary passed to the templating engine.
-html_context = {"versions_json_url": "https://elaspic2.gitlab.io/versions.json"}
+html_context = {"versions_json_url": "https://elaspic.gitlab.io/elaspic2/versions.json"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [
